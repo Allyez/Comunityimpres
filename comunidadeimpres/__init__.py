7fc93faa320a85dbf73ee29335523e7840a60221
Allyez <111750084+Allyez@users.noreply.github.com>
2024-09-22 21:10:57 -0300

    criando banco de dados __init__.py
    Adição do sqlalchemy, criação de uma engine e então a verificação de uma banco de dados existente. Se não houver, cria um

--- comunidadeimpres/__init__.py
+++ comunidadeimpres/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e63f212036e2e8d073cd893494282b98'
@@ -16,6 +17,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpres import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Banco de dados criado")
+else:
+    print('Banco de dados já existente')
 
 
 from comunidadeimpres import routes
